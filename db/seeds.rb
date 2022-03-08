531422356f31b66e662f9155f17391c8653e01df
sefty <seftyyunitasari@gmail.com>
2022-03-08 14:40:31 +0900

    Create Category model

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+Category.create(name: "Vegetables")
+Category.create(name: "Fruits")
+Category.create(name: "Grains")
+Category.create(name: "Spices")
+Category.create(name: "Carbs")
+Category.create(name: "Snacks")
+Category.create(name: "Fruits")
+Category.create(name: "Drinks")
+Category.create(name: "Body Cares")
+Category.create(name: "Others")
+
+
